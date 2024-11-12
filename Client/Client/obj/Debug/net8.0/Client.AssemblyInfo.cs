df52eff8811616dd731c18143046ebf846407f12
Serhii Olesh <serhiiolesh142@gmail.com>
2024-11-13 00:07:45 +0200

    test commit 21

--- Client/Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
+++ Client/Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a4b155fdf6f4b845a324fce20018626967e92a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
